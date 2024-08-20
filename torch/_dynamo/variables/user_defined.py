3ebc6e2a15130e5fc216c8278e42f94537f3ec81
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-08-20 19:16:32 +0800

    [dynamo] fix polyfill for user defined constructor `__new__`
    In `cls->tp_call`, if `cls->tp_new` does not return a instance of class `cls`, then `cls->tp_init` is not called on the new instance.
    
    ghstack-source-id: a7c521aa1db88b143a2df1ce6f2bd1def3b0342f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133822

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -393,9 +393,9 @@ class UserDefinedClassVariable(UserDefinedVariable):
             and hasattr(
                 self.value, "__exit__"
             )  # TODO(voz): These can invoke user code!
-            and self.value.__new__ == object.__new__
+            and self.is_standard_new()
             and SideEffects.cls_supports_mutation_side_effects(self.value)
-            and self.source is not None
+            and self.source
             and not is_forbidden_context_manager(self.value)
         ):
             # import here to avoid an unfortunate circular dependency.
@@ -523,7 +523,7 @@ class UserDefinedClassVariable(UserDefinedVariable):
         new_fn = inspect.getattr_static(self.value, "__new__", None)
         if isinstance(new_fn, staticmethod):
             new_fn = new_fn.__func__
-        return new_fn in (object.__new__, Generic.__new__)
+        return new_fn in {object.__new__, Generic.__new__}
 
     def call_hasattr(self, tx: "InstructionTranslator", name: str) -> "VariableTracker":
         if self.source:
